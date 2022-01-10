e82767e1a08d3e82aea1ec4412ad1550118ba465
Kamol Ibragimov <rusmirolub@gmail.com>
2022-01-10 00:27:38 +0000

    added Friend class

---
+++ Social-App/Data/Friend.h
@@ -0,0 +1,20 @@
+//
+//  Friend.h
+//  Social-App
+//
+//  Created by Komolbek Ibragimov on 10/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Friend : NSObject
+
+@property NSString *firstName;
+@property NSString *lastName;
+@property int age;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Social-App/Data/Friend.m
@@ -0,0 +1,12 @@
+//
+//  Friend.m
+//  Social-App
+//
+//  Created by Komolbek Ibragimov on 10/01/2022.
+//
+
+#import "Friend.h"
+
+@implementation Friend
+
+@end
