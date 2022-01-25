cf2a4a2620df3856f33ef91fd586d7348a2ed88c
Kamol Ibragimov <rusmirolub@gmail.com>
2022-01-25 17:41:03 +0000

    added UserAccount class empty methods

--- Social-App/Social-App/Model/Domain-Models/UserAccount.h
+++ Social-App/Social-App/Model/Domain-Models/UserAccount.h
@@ -6,10 +6,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "User.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UserAccount : NSObject
+
+#pragma mark - Public properties
+
+// readonly eliminates compiler to produce the property setter method, so property will not be able to be modified.
+@property (readonly, atomic, strong) User* user;
+
+#pragma mark - Public operations
+
+- (void) signInUsingUsername:(NSString *)username
+                    password:(NSString *)password;
+
+- (void) signUpUsingUsername:(NSString *)username
+                   firstName:(NSString *)name
+                 andPassword:(NSString *)password;
+
+- (void) signOut;
+
+- (void) deleteAccount;
+
+- (void) changeUsername:(NSString *)username;
+
+- (void) changePassword:(NSString *)password;
 
 @end
 
--- Social-App/Social-App/Model/Domain-Models/UserAccount.m
+++ Social-App/Social-App/Model/Domain-Models/UserAccount.m
@@ -9,4 +9,53 @@
 
 @implementation UserAccount
 
+# pragma mark - Initialisation
+
+- (id) init:(User *)user {
+    self = [super init];
+    
+    if (self) {
+        _user = user;
+    }
+    
+    return  self;
+}
+
+# pragma mark - Public operations
+
+- (void)signInUsingUsername:(NSString *)username
+                   password:(NSString *)password {
+    
+    
+}
+
+- (void)signUpUsingUsername:(NSString *)username
+                  firstName:(NSString *)name
+                andPassword:(NSString *)password {
+    
+}
+
+- (void)signOut {
+    
+}
+
+- (void) deleteAccount {
+    
+}
+
+- (void)changePassword:(NSString *)password {
+    
+}
+
+- (void)changeUsername:(NSString *)username {
+    
+}
+
+# pragma mark - Private operations
+
+- (BOOL) checkIdentity {
+    
+    return true;
+}
+
 @end
